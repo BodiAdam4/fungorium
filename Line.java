971bd8565792e064ce14829c6148837128b46fec
Drenyovszki Tamás <tamas.drenyovszki@edu.bme.hu>
2025-03-21 10:25:58 +0100

    Line class added

---
+++ Line.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Line 
+{
+    List<Tecton> connections;
+    int id;
+
+    Line(String name, Tecton t1, Tecton t2, int id)
+    {
+        Logger.Constructor(this, name, new Object[]{t1, t2, id});
+        this.id = id;
+        connections.add(t1);
+        connections.add(t2);
+        Logger.FunctionEnd();
+    }
+
+    boolean checkConnections()
+    {
+        return true;
+    }
+
+    void notifyNeighbors()
+    {
+
+    }
+
+    boolean growMushroom(Tecton to)
+    {
+        return true;
+    }
+
+    boolean growLine(Tecton to)
+    {
+        return true;
+    }
+}
