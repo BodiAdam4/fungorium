0f4c8f569468c9a249f7f989d8ee1920137ee411
Drenyovszki Tamás <tamas.drenyovszki@gmail.com>
2025-03-21 11:15:47 +0100

    :)

--- SporeContainer.java
+++ SporeContainer.java
@@ -1,3 +1,13 @@
-public class SporeContainer {
-    
+public class SporeContainer 
+{
+    public SporeContainer(String name)
+    {
+        Logger.Constructor(this, name);
+        Logger.FunctionEnd();
+    }
+
+    public boolean addSpores()
+    {
+        return true;
+    }
 }
