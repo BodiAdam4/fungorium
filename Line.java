e0386dec921e581738237e4dcfb10a148b294ca6
Baranyai Ferenc <baranyai.fer@gmail.com>
2025-03-22 10:48:16 +0100

    Fix line growth logic to correctly increment line identifier in growLine method

--- Line.java
+++ Line.java
@@ -86,7 +86,7 @@ public class Line
     public boolean growLine(Tecton to1, Tecton to2)
     {
         Logger.FunctionStart(this, "growLine", new Object[]{to1, to2});
-        Line nl = new Line("l" + (Integer.parseInt((Logger.GetObjectName(this).substring(1)) + 1)), to1, to2, id);
+        Line nl = new Line("l" + (Integer.parseInt((Logger.GetObjectName(this).substring(1))) + 1), to1, to2, id);
         return true;
     }
 
