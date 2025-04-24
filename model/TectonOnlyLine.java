b98e968c5d22f587e9ff2760050c0acedfb8b425
Drenyovszki Tamás <tamas.drenyovszki@gmail.com>
2025-04-24 22:33:47 +0200

    Implemented breakTecton() for Tecton's children and modified the test file connected to this method

--- model/TectonOnlyLine.java
+++ model/TectonOnlyLine.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.List;
+
+import listeners.ObjectChangeListener.ObjectChangeEvent;
+
 /**
  * A Tecton osztály leszármazottja, mely rendelkezik egy olyan különleges hatással, ami miatt csak egyfajta gombafonal tud kapcsolódni hozzá.
  */
@@ -28,6 +32,24 @@ public class TectonOnlyLine extends Tecton{
         else {
             System.out.println("You can't add lines with different id!");
             return false;
+        }
+    }
+
+
+    /**
+     * A tekton két tektonra törése
+     */
+    public void breakTecton(){
+        List<Tecton> ng = getNeighbors();
+        TectonOnlyLine t3 = new TectonOnlyLine();
+        changeListener.tectonChanged(ObjectChangeEvent.OBJECT_ADDED, t3);
+        for (Tecton t : ng) {
+            t3.setNeighbors(t);
+            t.setNeighbors(t3);
+        }
+        int size = connections.size();
+        for (int i = 0; i < size; i++) {
+            connections.get(0).Destroy();
         }
     }
 }
