7c6e36bbc9454aa526d4500ad9815cfaed797b81
geri2204 <i.am.your.ally.10@gmail.com>
2025-05-07 09:10:47 +0200

    Add GMushroom and PlayerPanel classes with initial implementation

---
+++ graphicObjects/GMushroom.java
@@ -0,0 +1,52 @@
+package graphicObjects;
+
+import listeners.MushroomListener;
+import model.Spore;
+import model.Line;
+
+
+/**
+ * Az osztály a modellbeli gombatestek megjelenítéséért felelős. 
+ * A játék során az osztály példányait a GTecton példányok fogják tartalmazni.
+*/
+public class GMushroom implements MushroomListener{
+    
+    /* - Publikus attribútumok*/
+    public String id;                   //A gombatesthez tartozó azonosító, amely alapján meg lehet találni a kontrollerben.
+    public String playerId;             //Annak a játékosnak az azonosítója akihez a gombatest tartozik.
+
+    /* - Konstruktor(ok)*/
+
+
+    /* - Getter/Setter metódusok*/
+
+
+    /* - Egyéb metódusok*/
+
+    /* - A grafikus gombatest megsemmisítésére alkalmas függvény.*/
+    public void destroy() {
+        //TODO: implementálni kell a gombatest megsemmisítését a grafikus felületen.
+    }
+
+
+    //MushroomListenert megvalósító metódusok
+
+
+    /* - Gombafonál növesztésekor lefutó függvény, paraméterként átadja a kinövesztett gombafonalat.*/
+    public void lineGrew(Line line) {
+        //TODO: implementálni kell a gombafonal növesztéséért a grafikus felületen.
+    }
+
+
+    /* - Gombaspóra dobásánál lefutó metódus, mely paraméterként megkapja az eldobott gombaspóra referenciáját.*/
+    public void sporeThrowed(Spore spore) {
+        //TODO: implementálni kell a gombaspóra dobását a grafikus felületen.
+    }
+
+
+    /* - A gombatest elpusztulásakor lefutó metódus.*/
+    public void mushroomDestroyed() {
+        //TODO: implementálni kell a gombatest elpusztulását a grafikus felületen.
+    }
+
+}
---
+++ graphicObjects/PlayerPanel.java
@@ -0,0 +1,47 @@
+package graphicObjects;
+
+
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+
+/**
+ * Egy-egy játékos beállítását lehetővé tevő UI elem. Ezen helyezkedik el a név beviteli mezeje, 
+ * illetve itt lehet kiválasztani a játékos színét és kasztját is.
+*/
+public class PlayerPanel {
+    
+    /* - Privát attribútumok*/
+    private JTextField nameBox;         //A játékos nevének beviteléhez szükséges szövegdoboz.
+    private Color color;                //A játékos színe, mely a későbbiekben segít megkülönböztetni a különböző objektumok tulajdonosait.
+    private JRadioButton insectRadio;   //Egy rádiógomb, mellyel eldönthető, hogy a játékos rovarász vagy gombász szeretne lenni.
+
+
+
+    /* - Konstruktor(ok)*/
+
+    /* - Getter/Setter metódusok*/
+
+    /* - Visszaadja a felhasználó által bevitt játékosnevet.*/
+    public String getName() {
+        return nameBox.getText();
+    }
+
+
+    /* - Visszaadja a játékos által kiválasztott színt*/
+    public Color getColor(){
+        return color;
+        //TODO: Lehetséges, hogy a színt nem így kellene visszaadni.
+    }
+
+
+    /* - Visszaad egy logikai változót, ami megmondja, hogy a játékos rovarász vagy gombász kasztot választotta.*/
+    public boolean isInsect(){
+        return insectRadio.isSelected();
+    }
+
+
+    /* - Egyéb metódusok*/
+
+}
