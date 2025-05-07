9e827648ce5f07554401af4763bc28f5bc079009
Bódi Ádám <madaidob@gmail.com>
2025-05-07 19:35:52 +0200

    Add TectonListener interface for monitoring Tecton changes

---
+++ listeners/TectonListener.java
@@ -0,0 +1,28 @@
+package listeners;
+
+import model.Mushroom;
+import model.Tecton;
+
+/**
+ * A TectonListener interfész a tektonokon bekövetkező változások figyelését valósítja meg. 
+ * Az interfész tartalmaz minden lehetséges eseményhez egy függvényt, amit a tekton lefuttat, ha bekövetkezik.
+ */
+public interface TectonListener {
+    
+    /**
+     *  A tektonon való gombatest növesztésének következtében lefutó metódus. Paraméterként megkapja a kinövesztett gombatestet.
+     * @param mushroom a kinövesztett gombatestet adja meg
+     */
+    public void mushroomAdded(Mushroom mushroom);
+
+    /**
+     * A tektonon lévő gombatest megszűnésének következtében lefutó metódus.
+     */
+    public void mushroomRemoved();
+    
+    /**
+     * A tekton eltörésekor lefutó metódus, mely paraméterként átadja a másik tektont, ami a törés során keletkezett.
+     * @param tecton a törés során keletkezett tektont adja meg
+     */
+    public void tectonBroken(Tecton tecton);
+}
