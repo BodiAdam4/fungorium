4f1dc9161b2aaea8bd15ad41c65f7fdb086d10d2
Gurzó Bence <gurzobence24@gmail.com>
2025-05-07 21:42:15 +0200

    I implemented the InsectListener class

---
+++ listeners/InsectListener.java
@@ -0,0 +1,25 @@
+package listeners;
+import model.Tecton;
+
+
+/**
+ * Az InsectListener interfész a rovarokon bekövetkező változások figyelését valósítja meg. 
+ * Az interfész tartalmaz minden lehetséges eseményhez egy függvényt, amit a rovar lefuttat, ha bekövetkezik.
+*/
+public interface InsectListener {
+
+    /* - Azon metódus, amely akkor hívodik meg ha egy rovar elkezd mozogni.*/
+    public void moveStarted(Tecton to);
+
+    /* - Azon metódus, amely akkor hívódik meg ha egy rovar befejezte a mozgását */
+    public void moveFinished(Tecton to);
+
+    /* - Azon metódus, amely akkor hívódik meg ha egy rovar elfogyaszt egy spórát */
+    public void sporeEaten();
+
+    /* - Azon metódus, amely akkor hívódik meg ha egy spóra hatása lejár egy rovaron */
+    public void effectReseted();
+
+    /* - Azon metódus, amely akkor hívódik meg amikor egy rovar elpusztul */
+    public void insectDestroyed();
+}
