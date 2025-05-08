9a842b824a5da99a9f8871734a0d40b6edd8c728
Tamás Drenyovszki <tamas.drenyovszki@gmail.com>
2025-05-09 01:12:47 +0200

    modified comments in LineListener

--- listeners/LineListener.java
+++ listeners/LineListener.java
@@ -6,17 +6,27 @@ import model.Insect;
 /**
  * A LineListener interfész a gombafonalakon bekövetkező változások figyelését valósítja meg. 
  * Az interfész tartalmaz minden lehetséges eseményhez egy függvényt, amit a gombafonal lefuttat, ha bekövetkezik.
-*/
+ */
 public interface LineListener {
 
-    /* - Azon metódus, amely egy rovar elfogyasztása után hívódik meg.*/
+    /** 
+     * Rovar elfogyasztásakor lefutó függvény
+     * @param insect a megevett rovar
+     */
     public void insectEaten(Insect insect);
 
     
-    /* - A gombafonal elpusztulásakor meghívódó metódus.*/
+    /**
+     * A gombafonal elpusztulásakor meghívódó metódus.
+     */
     public void lineDestroyed();
 
 
-    /* - A gombafonal fázisainak változása esetén fut le a függvény. Három különböző fázis követése lehetséges,amit a paraméterként adott szám mutat meg. Ha a phase = 1, akkor a gombafonal kinőtt, ha 2 akkor a gombafonal elkezdett haldokolni, és ha 0 akkor még csak most kezdett el  kinőni.*/
+    /** 
+     * A gombafonal fázisainak változása esetén fut le a függvény. 
+     * Három különböző fázis követése lehetséges,amit a paraméterként adott szám mutat meg. 
+     * Ha a phase = 1, akkor a gombafonal kinőtt, ha 2 akkor a gombafonal elkezdett haldokolni, és ha 0 akkor még csak most kezdett el kinőni.
+     * @param phase a fázist mutató szám
+     */
     public void phaseChange(int phase);
 }
