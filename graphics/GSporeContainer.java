b635bca67a290d0a6614d2183ca27a4bf9a824c8
Bódi Ádám <madaidob@gmail.com>
2025-05-08 13:55:16 +0200

    Implement GSporeContainer class with SporeContainerListener interface for spore management

--- graphics/GSporeContainer.java
+++ graphics/GSporeContainer.java
@@ -1,5 +1,49 @@
 package graphics;
 
-public class GSporeContainer {
+import listeners.SporeContainerListener;
+/**
+ * A tektonon elhelyezkedő spórák kirajzolásáért felelős osztály.
+ * A játék során a példányait a GTecton osztály példányai fogják tartalmazni, mivel egy tektonhoz egy spóratároló tartozik.
+ */
+public class GSporeContainer extends Image implements SporeContainerListener {
     
+    /* - Publikus attribútumok*/
+    public String id;                   //A spóratárolóhoz tartozó azonosító, amely alapján meg lehet találni a kontrollerben
+    public String tectonId;             //Annak a tektonnak az azonosítója amihez a spóratároló tartozik.
+
+    /* - Konstruktor(ok)*/
+    public GSporeContainer(String id, String tectonId) {
+        super("graphics/images/Spores.png"); // A SporeContainer képe
+        this.id = id;
+        this.tectonId = tectonId;
+    }
+
+    /* - Getter/Setter metódusok*/
+
+
+    /* - Egyéb metódusok*/
+
+    /**
+     * A grafikus spóratároló megsemmisítésére szolgáló függvény.
+     */
+    public void destroy(){
+
+    }
+
+    /**
+     * A spóratárolóhoz gombaspóra hozzáadása esetén lefutó függvény, paraméterként átadja a spóratárolóban lévő spórák számát
+     */
+    @Override
+    public void sporeAdded(int sporeCount) {
+        
+    }
+
+    /**
+     * A spóratárolóból gombaspóra elvétele esetén lefutó függvény, paraméterként átadja a spóratárolóban lévő spórák számát.
+     */
+    @Override
+    public void sporeRemoved(int sporeCount) {
+        
+    }
+
 }
