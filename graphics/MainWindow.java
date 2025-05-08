890acf62054d22fabd6bdbd4276adbd061a63bf4
geri2204 <i.am.your.ally.10@gmail.com>
2025-05-08 20:45:54 +0200

    Enhance MainWindow: Add result panel with game over message and results display, implement notification panel, and improve layout handling for better UI experience.

--- graphics/MainWindow.java
+++ graphics/MainWindow.java
@@ -10,13 +10,17 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.HashMap;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JTextArea;
 import javax.swing.OverlayLayout;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
@@ -191,9 +195,9 @@ public class MainWindow extends JFrame implements JobListener{
         mainContentPanel.add(controlPanel, BorderLayout.EAST); //A vezérlő panel hozzáadása a fő tartalom panelhez
 
 
-        //TODO: Ezt majd el kell innen pakolni
+        //###############################################################################
 
-        // JLayeredPane
+        //JLayeredPane az egymásrailleszthetőséghez
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setLayout(null); // Fontos: nincs LayoutManager
         layeredPane.setBounds(0, 0, this.getWidth(), this.getHeight());
@@ -201,32 +205,135 @@ public class MainWindow extends JFrame implements JobListener{
         // Méretezhető fő tartalom panel
         mainContentPanel.setBounds(0, 0, this.getWidth(), this.getHeight());
         layeredPane.add(mainContentPanel, JLayeredPane.DEFAULT_LAYER);
+    
+        //###############################[resultPanel]#################################
 
-        // Piros overlay panel
+        //JPanel az eredményhirdetéshez
+        JPanel resultPanel = new JPanel(); //Az eredményhirdetés panel inicializálása
+        //resultPanel.setBackground(Color.GREEN);
+        resultPanel.setBackground(null); //Az eredményhirdetés panel háttérszínének beállítása
+        resultPanel.setOpaque(false);
+        resultPanel.setOpaque(true);
+        resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS)); //Az eredményhirdetés panel elrendezése
+        resultPanel.setBounds(0, 0, 700, 500); //Az eredményhirdetés panel méretének beállítása
+        //TODO: itt lehet beállítnai a láthatóságát, hogy kezdetben ne legyen látható
+        resultPanel.setVisible(false); // Kezdetben látható
+        layeredPane.add(resultPanel, JLayeredPane.PALETTE_LAYER); //Az eredményhirdetés panel hozzáadása a rétegelt panelhez
+
+        // Görgethető eredmény panel (ScrollPane)
+        JPanel resultContent = new JPanel();
+        resultContent.setLayout(new BoxLayout(resultContent, BoxLayout.Y_AXIS));
+        resultContent.setBackground(Color.DARK_GRAY);
+
+        // "Game Over" cím
+        JLabel gameOverLabel = new JLabel("GAME OVER");
+        gameOverLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        gameOverLabel.setForeground(Color.RED);
+        gameOverLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 10)));
+        resultContent.add(gameOverLabel);
+
+        // "Results" felirat
+        JLabel resultsLabel = new JLabel("Results:");
+        resultsLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        resultsLabel.setForeground(Color.WHITE);
+        resultsLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 10)));
+        resultContent.add(resultsLabel);
+
+        // "Top Mushroompickers" felirat
+        JLabel topMushLabel = new JLabel("Top Mushroompickers:");
+        topMushLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        topMushLabel.setForeground(Color.WHITE);
+        topMushLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 10)));
+        resultContent.add(topMushLabel);
+
+        // Eredmények szöveges megjelenítése (Mushroompickers)
+        JTextArea mushroomResults = new JTextArea("# 1.: MushroomPicker_2__________________19\n# 2.: MushroomPicker_1__________________17\n# 3.: MushroomPicker_3__________________12\n# 4.: MushroomPicker_4__________________10\n# 5.: MushroomPicker_5__________________8\n# 6.: MushroomPicker_6__________________5\n# 7.: MushroomPicker_7__________________3\n# 8.: MushroomPicker_8__________________2");
+        mushroomResults.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        mushroomResults.setEditable(false);
+        mushroomResults.setBackground(Color.DARK_GRAY);
+        mushroomResults.setForeground(Color.WHITE);
+        mushroomResults.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 5)));
+        resultContent.add(mushroomResults);
+
+        // Szeparátor
+        JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+        separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1));
+        resultContent.add(Box.createRigidArea(new Dimension(0, 10)));
+        resultContent.add(separator);
+
+        // "Top Insectpickers" felirat
+        JLabel topInsectLabel = new JLabel("Top Insectpickers:");
+        topInsectLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        topInsectLabel.setForeground(Color.WHITE);
+        topInsectLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 10)));
+        resultContent.add(topInsectLabel);
+
+        // Eredmények szöveges megjelenítése (Insectpickers)
+        JTextArea insectResults = new JTextArea("# 1.: InsectPicker_1____________________8\n# 2.: InsectPicker_2____________________5\n# 3.: InsectPicker_3____________________4\n# 4.: InsectPicker_4____________________3\n# 5.: InsectPicker_5____________________2\n# 6.: InsectPicker_6____________________1\n# 7.: InsectPicker_7____________________0\n# 8.: InsectPicker_8____________________0");
+        insectResults.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        insectResults.setEditable(false);
+        insectResults.setBackground(Color.DARK_GRAY);
+        insectResults.setForeground(Color.WHITE);
+        insectResults.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultContent.add(Box.createRigidArea(new Dimension(0, 5)));
+        resultContent.add(insectResults);
+
+        // Exit Game gomb
+        JButton exitButton = new JButton("Exit game");
+        exitButton.setFont(new Font("Arial", Font.BOLD, 16));
+        exitButton.setBackground(Color.DARK_GRAY);
+        exitButton.setForeground(Color.RED);
+        exitButton.setFocusPainted(false);
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        exitButton.setMaximumSize(new Dimension(200, 40));
+        exitButton.addActionListener(e -> System.exit(0)); // Exit event added
+        resultContent.add(Box.createRigidArea(new Dimension(0, 20)));
+        resultContent.add(exitButton);
+
+        // ScrollPane, ha túl sok tartalom lenne
+        JScrollPane scrollPane2 = new JScrollPane(resultContent);
+        scrollPane2.setBounds(0, 0, resultPanel.getWidth(), resultPanel.getHeight());
+        scrollPane2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane2.setBorder(null);
+
+        // Hozzáadás a resultPanel-hez
+        resultPanel.setLayout(null); // Használjunk abszolút pozícionálást itt
+        resultPanel.add(scrollPane2);
+
+        //###########################[notificJPanel]####################################
+
+        //A notification panel létrehozása
         JPanel notificJPanel = new JPanel();
         notificJPanel.setBackground(Color.DARK_GRAY);
         notificJPanel.setOpaque(true);
         notificJPanel.setLayout(new BoxLayout(notificJPanel, BoxLayout.Y_AXIS));
         notificJPanel.setBounds(0, 0, 600, 60);
-        notificJPanel.setVisible(true); // Kezdetben látható
         notificJPanel.add(Box.createRigidArea(new Dimension(0, 5))); // 5 pixel magas üres hely
+        //TODO: itt lehet beállítnai a láthatóságát, hogy kezdetben ne legyen látható
+        notificJPanel.setVisible(false); // Kezdetben látható
         layeredPane.add(notificJPanel, JLayeredPane.PALETTE_LAYER);
 
-        // JLabel a notification címhez (felül)
+        //JLabel a notification címhez (felül)
         JLabel notificationLabel = new JLabel("Notification Bar");
         notificationLabel.setForeground(Color.RED);
         notificationLabel.setFont(new Font("Arial", Font.BOLD, 18));
         notificationLabel.setBorder(new EmptyBorder(5, 5, 0, 0)); // Top, Left, Bottom, Right
         notificationLabel.setAlignmentX(Component.LEFT_ALIGNMENT); // Balra igazítás a panelen belül (vagy CENTER_ALIGNMENT, ha középre kéred)
 
-        // JLabel az üzenet szövegéhez (alatta)
+        //JLabel az üzenet szövegéhez (alatta)
         notificationText = new JLabel("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun!");
         notificationText.setForeground(Color.WHITE);
         notificationText.setFont(new Font("Arial", Font.ITALIC, 14));
         notificationText.setBorder(new EmptyBorder(0, 5, 0, 0)); // Top, Left, Bottom, Right
         notificationText.setAlignmentX(Component.LEFT_ALIGNMENT); // Igazítás megegyezően
 
-        // Hozzáadás a panelhez
+        //Hozzáadás a panelhez
         notificJPanel.add(notificationLabel);
         notificJPanel.add(Box.createRigidArea(new Dimension(5, 5))); // Kis térköz a két szöveg között
         notificJPanel.add(notificationText);
@@ -234,7 +341,7 @@ public class MainWindow extends JFrame implements JobListener{
 
         this.add(layeredPane, BorderLayout.CENTER);
 
-        // Resize listener
+        //Resize listener
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -250,13 +357,21 @@ public class MainWindow extends JFrame implements JobListener{
                 controlPanel.setPreferredSize(new Dimension(width / 4, height));
                 controlPanel.setSize(width / 4, height);
         
-                // Dinamikus pozícionálás az alsó részre
-                int stateWidth = 700;
-                int stateHeight = 70;
-                int stateX = (width/2)-(stateWidth/2); // 50 pixel távolság a jobb széltől
-                int stateY = height - stateHeight - 50;
+                //Az értesítéspanel pozícionálása az alsó részre
+                int notWidth = 700;
+                int notHeight = 70;
+                int stateX = (width/2)-(notWidth/2); // 50 pixel távolság a jobb széltől
+                int stateY = height - notHeight - 50;
         
-                notificJPanel.setBounds(stateX, stateY, stateWidth, stateHeight);
+                notificJPanel.setBounds(stateX, stateY, notWidth, notHeight);
+
+                //Az eredményhirdetés panel pozicionálása
+                int resultWidth = 700;
+                int resultHeight = 500;
+                int resultX = (width/2)-(resultWidth/2); // 50 pixel távolság a jobb széltől
+                int resultY = (height/2)-(resultHeight/2); // 50 pixel távolság a jobb széltől
+
+                resultPanel.setBounds(resultX, resultY, resultWidth, resultHeight);
         
                 layeredPane.revalidate();
                 layeredPane.repaint();
