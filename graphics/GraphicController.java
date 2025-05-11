324c54e11ba0edc584836837c4756429c81a3a9c
Hanry <mapokhanrynek@gmail.com>
2025-05-12 00:18:49 +0200

    Add GraphicController class

---
+++ graphics/GraphicController.java
@@ -0,0 +1,12 @@
+package graphics;
+/**
+ * Az GraphicController osztály felelős a játékmenet során a kontroller és a grafikus felület összeköttetéséért.
+ * A modelbeli objektumok létrejöttekor itt történik a grafikus objektumok létrehozása és a különböző eseményfigyelők beállítása.
+ */
+public class GraphicController {
+    /* - Publikus attribútumok*/
+    /* - Privát attribútumok*/
+    /* - Konstruktor(ok)*/
+    /* - Getter/Setter metódusok*/
+    /* - Egyéb metódusok*/
+}
